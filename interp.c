763648bdf1b1b8f6298e656036d7d4cc4d6a06a7
Clayton Richey <17693233+Odoth@users.noreply.github.com>
2013-02-20 10:57:06 -0700

    Removed 'sing' feature.

--- interp.c
+++ interp.c
@@ -124,7 +124,6 @@ const   struct  cmd_type    cmd_table   [] =
     { "order",      do_order,   POS_RESTING,     0,  LOG_NORMAL, 1, FALSE, FALSE  },
     { "practice",   do_practice,    POS_SLEEPING,    0,  LOG_NORMAL, 1, FALSE, FALSE  },
     { "rest",       do_rest,    POS_SLEEPING,    0,  LOG_NORMAL, 1, FALSE, TRUE  },
-    { "sing",       do_sing,    POS_RESTING,       0,    LOG_NORMAL, 1, FALSE, TRUE  },
     { "sit",        do_sit,     POS_SLEEPING,    0,  LOG_NORMAL, 1, FALSE, TRUE  },
     { "sockets",    do_sockets, POS_DEAD,       L4,  LOG_NORMAL, 1, FALSE, FALSE  },
     { "stand",      do_stand,   POS_SLEEPING,    0,  LOG_NORMAL, 1, FALSE, TRUE  },
