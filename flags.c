42fe8cc45e488b9efde6c3e285d6354c26a30b2c
Clayton Richey <clayton.richey@gmail.com>
2013-02-22 01:30:55 -0700

    Can turn pkill off after 90 days. Expire is set for already PK chars at login.

--- flags.c
+++ flags.c
@@ -27,7 +27,6 @@
 
 #if defined(macintosh)
 #include <types.h>
-#include <time.h>
 #else
 #include <sys/types.h>
 #if !defined(WIN32)
@@ -39,6 +38,7 @@
 #include <stdlib.h>
 #include "merc.h"
 #include "tables.h"
+#include <time.h>
 
 int flag_lookup args( ( const char *name, const struct flag_type *flag_table) );
 
@@ -277,6 +277,165 @@ void do_pkil( CHAR_DATA *ch, char *argument)
     send_to_char("You must type 'pkill on' to turn on pkill, or 'pkill grade' to view grading system.\n\r",ch);
 }
 
+void print_pkill_grades( CHAR_DATA *ch )
+{
+        int i;
+        char buf[MSL];
+
+        send_to_char("{D         Min     Earned     Death Loss{x\n\r", ch );
+        send_to_char("{DGrade    Pts    for kill     pk    wf{x\n\r", ch );
+
+        for( i=1; pkgrade_table[i].pkpoints > 0; i++ )
+        {
+            sprintf( buf, "  %s    %5d    %5d     %5d  %5d\n\r",
+                pkgrade_table[i].grade, pkgrade_table[i].pkpoints,
+                pkgrade_table[i].earned, pkgrade_table[i].lost,
+                pkgrade_table[i].lost_in_warfare );
+            send_to_char( buf, ch );
+        }
+            sprintf( buf, "  %s    %5d    %5d     %5d  %5d\n\r",
+                pkgrade_table[i].grade, pkgrade_table[i].pkpoints,
+                pkgrade_table[i].earned, pkgrade_table[i].lost,
+                pkgrade_table[i].lost_in_warfare );
+            send_to_char( buf, ch );
+}
+
+try_set_hardcore(CHAR_DATA *ch)
+{
+    if ( !IS_SET(ch->act, PLR_PERM_PKILL) )
+    {
+	send_to_char( "You must turn normal pkill on first!\n\r", ch );
+	return;
+    }
+
+    if ( IS_SET(ch->act, PLR_HARDCORE) )
+    {
+	send_to_char( "You are already a hardcore pkiller.\n\r", ch );
+	return;
+    }
+	
+    send_to_char("Ok. Hope you know what you're up to.\n\r", ch );
+    send_to_char("Type 'pkill confirm <password>' to confirm your intentions.\n\r",ch);
+    send_to_char("Typing 'pkill' with no argument or with any argument other than 'confirm'\n\r", ch);
+    send_to_char("will abort this action.  Please read 'HELP PKILL' before you decide.\n\r", ch);
+    ch->pcdata->confirm_pkill = TRUE;
+    wiznet("$N is about to turn on $S hardcore flag.",ch, NULL, WIZ_FLAGS, 0, 0);
+
+}
+
+pkill_confirm(CHAR_DATA *ch, char *arg)
+{
+   if (ch->pcdata->confirm_pkill)
+   {
+    /* extra password check to prevent pkill by trigger-abuse */
+	if ( strcmp(crypt(arg, ch->pcdata->pwd), ch->pcdata->pwd) )
+	{
+	    send_to_char( "Wrong password, try again.\n\r", ch );
+	    return; 
+	}
+
+	if ( !IS_SET(ch->act, PLR_PERM_PKILL) )
+	{
+	    SET_BIT(ch->act, PLR_PERM_PKILL);
+	    reset_pkill_expire(ch);
+	    sprintf(log_buf, "%s has declared %sself to be a permanent pkiller!",
+	    ch->name, (ch->pcdata->true_sex == SEX_FEMALE) ? "her" : "him");
+	    info_message(ch, log_buf, TRUE);
+	    log_string( log_buf );
+	    wiznet("$N has turned on $S pkill flag.", ch, NULL, WIZ_FLAGS, 0, 0);
+	}
+	else
+	{
+	    SET_BIT(ch->act, PLR_HARDCORE);
+	    reset_pkill_expire(ch);
+	    sprintf(log_buf, "%s has declared %sself to be a hardcore pkiller!",
+	    ch->name, (ch->pcdata->true_sex == SEX_FEMALE) ? "her" : "him");
+	    info_message(ch, log_buf, TRUE);
+	    log_string( log_buf );
+	    wiznet("$N has turned on $S hardcore flag.", ch, NULL, WIZ_FLAGS, 0, 0);
+	}
+        /* Pulled from flags.c and moved over here. Stop people from turning on
+            pkill just to get a cheap kill. - Astark 12-23-12 */
+        if ( ch->pcdata != NULL )
+            ch->pcdata->pkill_timer = -10 * PULSE_VIOLENCE;
+
+	update_bounty(ch);
+	    
+    }
+    else
+	send_to_char("Pkill activation was aborted.\n\r",ch);
+	
+    ch->pcdata->confirm_pkill = FALSE;
+}	
+
+void reset_pkill_expire( CHAR_DATA *ch)
+{
+	if ( IS_NPC(ch) )
+	    return;
+
+        struct tm *timeinfo;
+        timeinfo=localtime( &current_time);
+        timeinfo->tm_mday+=90;
+
+	ch->pcdata->pkill_expire=mktime(timeinfo);
+}
+
+void try_set_pkill(CHAR_DATA *ch)
+{
+    if ( IS_SET(ch->act, PLR_PERM_PKILL) )
+    {
+	send_to_char("You are already a pkiller.  Hope that's what you wanted.\n\r",ch);
+	send_to_char("Typing 'pkill hardcore' followed by 'pkill confirm' will turn you\n\r", ch);
+	send_to_char("into a hardcore pkiller.'\n\r", ch);
+	ch->pcdata->confirm_pkill = FALSE;
+	return;
+    }
+    else
+    {
+	send_to_char("WARNING: This command is permanent -- you can NOT EVER TURN PKILL BACK OFF!\n\r\n\r",ch);
+	send_to_char("Type 'pkill confirm <password>' to confirm your intentions.\n\r",ch);
+	send_to_char("Typing 'pkill' with no argument or with any argument other than 'confirm'\n\r", ch);
+	send_to_char("will abort this action.  Please read 'HELP PKILL' before you decide.\n\r", ch);
+	ch->pcdata->confirm_pkill = TRUE;
+	wiznet("$N is about to turn on $S pkill flag.",ch, NULL, WIZ_FLAGS, 0, 0);
+    }
+}
+
+void try_remove_pkill( CHAR_DATA *ch)
+{
+    if (ch->class == class_lookup("assassin") )
+    {
+	send_to_char("Assassins cannot remove pkill status.\n\r", ch );
+	return;
+    }
+    REMOVE_BIT( ch->act, PLR_PERM_PKILL );
+    REMOVE_BIT( ch->act, PLR_HARDCORE );
+    
+    send_to_char("You are no longer a player killer.\n\r", ch );
+
+}
+
+void print_pkill_expire( CHAR_DATA *ch)
+{
+    if (ch->class == class_lookup("assassin") )
+    {
+        send_to_char("Assassins cannot remove pkill status.\n\r", ch );
+        return;
+    }
+    
+    if (! IS_SET( ch->act, PLR_PERM_PKILL ) )
+    {
+	send_to_char("You are not a player killer.\n\r",ch);
+	return;
+    }
+
+    char ts_buf[80];
+    strftime(ts_buf,sizeof(ts_buf),"%x %X", localtime(&(ch->pcdata->pkill_expire)));
+
+    printf_to_char( ch, "You can remove PK after %s.\n\r", 
+	ts_buf );
+
+}
 
 /* Pkill code by Rimbol, 7/20/97 */
 void do_pkill( CHAR_DATA *ch, char *argument)
@@ -289,133 +448,47 @@ void do_pkill( CHAR_DATA *ch, char *argument)
     argument = one_argument_keep_case( argument, arg1 );
     argument = one_argument_keep_case( argument, arg2 );
 
-    if( !str_prefix(arg1, "grades") )
+    if( !str_cmp(arg1, "grades") )
     {
-	int i;
-	char buf[MSL];
-
-	send_to_char("{D         Min     Earned     Death Loss{x\n\r", ch );
-	send_to_char("{DGrade    Pts    for kill     pk    wf{x\n\r", ch );
-
-	for( i=1; pkgrade_table[i].pkpoints > 0; i++ )
-	{
-	    sprintf( buf, "  %s    %5d    %5d     %5d  %5d\n\r",
-		pkgrade_table[i].grade, pkgrade_table[i].pkpoints,
-		pkgrade_table[i].earned, pkgrade_table[i].lost,
-		pkgrade_table[i].lost_in_warfare );
-	    send_to_char( buf, ch );
-	}
-	    sprintf( buf, "  %s    %5d    %5d     %5d  %5d\n\r",
-		pkgrade_table[i].grade, pkgrade_table[i].pkpoints,
-		pkgrade_table[i].earned, pkgrade_table[i].lost,
-		pkgrade_table[i].lost_in_warfare );
-	    send_to_char( buf, ch );
-
+	print_pkill_grades(ch);
 	return;
     }
 
-    if ( IS_SET(ch->act, PLR_HARDCORE) )
-    {
-	send_to_char( "You are already a hardcore pkiller.\n\r", ch );
-	return;
-    }
-	
-    if ( IS_SET(ch->act, PLR_PERM_PKILL) && arg1[0] == '\0' )
-    {
-	send_to_char("You are already a pkiller.  Hope that's what you wanted.\n\r",ch);
-	send_to_char("Typing 'pkill hardcore' followed by 'pkill confirm' will turn you\n\r", ch);
-	send_to_char("into a hardcore pkiller.'\n\r", ch);
-	ch->pcdata->confirm_pkill = FALSE;
-	return;
-    }
-	
     if (ch->level < 10 && ch->pcdata->remorts == 0)
     {
-	send_to_char("You must reach level 10 before you may choose to be a pkiller.\n\r",ch);
-	return;
+        send_to_char("You must reach level 10 before you may alter your pkill status.\n\r",ch);
+        return;
     }
+	if ( !strcmp( arg1, "time") )
+	{
+		print_pkill_expire(ch);
+		return;
+	}
 	
-    if (ch->pcdata->confirm_pkill)
-    {
-	if (strcmp(arg1, "confirm") == 0)
+	if ( !strcmp( arg1, "off") )
 	{
-	    /* extra password check to prevent pkill by trigger-abuse */
-	    if ( strcmp(crypt(arg2, ch->pcdata->pwd), ch->pcdata->pwd) )
-	    {
-		send_to_char( "Wrong password, try again.\n\r", ch );
-		return; 
-	    }
-
-	    if ( !IS_SET(ch->act, PLR_PERM_PKILL) )
-	    {
-		SET_BIT(ch->act, PLR_PERM_PKILL);
-		sprintf(log_buf, "%s has declared %sself to be a permanent pkiller!",
-			ch->name, (ch->pcdata->true_sex == SEX_FEMALE) ? "her" : "him");
-		info_message(ch, log_buf, TRUE);
-		log_string( log_buf );
-		wiznet("$N has turned on $S pkill flag.", ch, NULL, WIZ_FLAGS, 0, 0);
-
-                /* Pulled from flags.c and moved over here. Stop people from turning on
-                   pkill just to get a cheap kill. - Astark 12-23-12 */
-                if ( ch->pcdata != NULL )
-                    ch->pcdata->pkill_timer = -10 * PULSE_VIOLENCE;
-
-		update_bounty(ch);
-	    }
-	    else
-	    {
-		SET_BIT(ch->act, PLR_HARDCORE);
-		sprintf(log_buf, "%s has declared %sself to be a hardcore pkiller!",
-			ch->name, (ch->pcdata->true_sex == SEX_FEMALE) ? "her" : "him");
-		info_message(ch, log_buf, TRUE);
-		log_string( log_buf );
-		wiznet("$N has turned on $S hardcore flag.", ch, NULL, WIZ_FLAGS, 0, 0);
-
-                /* Pulled from flags.c and moved over here. Stop people from turning on
-                   pkill just to get a cheap kill. - Astark 12-23-12 */
-                if ( ch->pcdata != NULL )
-                    ch->pcdata->pkill_timer = -10 * PULSE_VIOLENCE;
-
-		update_bounty(ch);
-	    }
+		try_remove_pkill(ch);
+		return;
 	}
-	else
-	    send_to_char("Pkill activation was aborted.\n\r",ch);
-	
-	ch->pcdata->confirm_pkill = FALSE;
-    }
-    else if (strcmp(arg1, "on") == 0)
-    {
-	if ( IS_SET(ch->act, PLR_PERM_PKILL) )
+	if ( !strcmp( arg1, "on" ) )
 	{
-	    send_to_char( "You are already a pkiller.\n\r", ch );
-	    send_to_char( "Type 'pkill hardcore' if you wish to become a hardcore pkiller.\n\r", ch );
-	    return;
+		try_set_pkill(ch);
+		return;
 	}
-	send_to_char("WARNING: This command is permanent -- you can NOT EVER TURN PKILL BACK OFF!\n\r\n\r",ch);
-	send_to_char("Type 'pkill confirm <password>' to confirm your intentions.\n\r",ch);
-	send_to_char("Typing 'pkill' with no argument or with any argument other than 'confirm'\n\r", ch);
-	send_to_char("will abort this action.  Please read 'HELP PKILL' before you decide.\n\r", ch);
-	ch->pcdata->confirm_pkill = TRUE;
-	wiznet("$N is about to turn on $S pkill flag.",ch, NULL, WIZ_FLAGS, 0, 0);
-    }
-    else if (strcmp(arg1, "hardcore") == 0)
-    {
-	if ( !IS_SET(ch->act, PLR_PERM_PKILL) )
+	if ( !strcmp( arg1, "hardcore") )
 	{
-	    send_to_char( "You must turn normal pkill on first!\n\r", ch );
-	    return;
+		try_set_hardcore(ch);
+		return;
+	}
+	if ( !strcmp( arg1, "confirm") )
+	{
+		pkill_confirm(ch, arg2);
+		return;
 	}
 
-	send_to_char("Ok. Hope you know what you're up to.\n\r", ch );
-	send_to_char("Type 'pkill confirm <password>' to confirm your intentions.\n\r",ch);
-	send_to_char("Typing 'pkill' with no argument or with any argument other than 'confirm'\n\r", ch);
-	send_to_char("will abort this action.  Please read 'HELP PKILL' before you decide.\n\r", ch);
-	ch->pcdata->confirm_pkill = TRUE;
-	wiznet("$N is about to turn on $S hardcore flag.",ch, NULL, WIZ_FLAGS, 0, 0);
-    }
-    else
-	send_to_char("Type 'pkill on' to activate pkill.  Please read 'HELP PKILL' first.\n\r",ch);
+	/* nothing caught... bad syntax */
+	send_to_char( "Usage: pkill <grade|on|off|hardcore|confirm|time>\n\r", ch );
+	  
 }
 
 /* roleplay flag by Bobble */
