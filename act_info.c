1046c9a3a6377f66591af0a618da19e4d308bb6e
bobble <bobble@localhost>
2013-02-23 16:46:17 +1300

    included figure for pre-reduction bonus in stat display

--- act_info.c
+++ act_info.c
@@ -5274,6 +5274,7 @@ void do_attributes( CHAR_DATA *ch, char *argument )
 /* Show Str..Luc bonuses as a bar graph -- Bobble 02/2013 */
 void print_stat_bars( CHAR_DATA *ch, BUFFER *output )
 {
+    const int MAX_COST = 8;
     char bar_buf[256], buf[MAX_STRING_LENGTH];
     int bar_next;
     int si, bonus, cost, partial, i;
@@ -5287,7 +5288,7 @@ void print_stat_bars( CHAR_DATA *ch, BUFFER *output )
         // generate string for bar graph
         bar_next = 0;
         color_switched = FALSE;
-        for (cost = 1; cost <= 9; cost++)
+        for (cost = 1; cost <= MAX_COST; cost++)
         {
             for (i = 0; i < 5; i++)
             {
@@ -5302,15 +5303,17 @@ void print_stat_bars( CHAR_DATA *ch, BUFFER *output )
                 }
                 bar_buf[bar_next++] = '0' + partial;
             }
-            if (cost < 9)
+            if (cost < MAX_COST)
                 bar_buf[bar_next++] = ' ';
         }
         // terminate string
         bar_buf[bar_next++] = 0;
         // now send it to output
-        sprintf( buf, "{D|{x {b%s:{x %3d(%3d)  [{g%s{x]    {D|{x\n\r"
+        sprintf( buf, "{D|{x {b%s: {x%3d %s %3d => %3d  [{g%s{x]  {D|{x\n\r"
             , stat_table[si].abbreviation
             , ch->perm_stat[stat->stat]
+            , ch->mod_stat[stat->stat] < 0 ? "-" : "+"
+            , ABS(ch->mod_stat[stat->stat])
             , get_curr_stat(ch,stat->stat)
             , bar_buf
         );
