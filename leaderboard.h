e1700f08e241639e7b978abc2a380fd7c3a3def8
Vodur <Vodur@localhost>
2013-03-12 08:30:27 +0300

    Fixed formatting with vim.

--- leaderboard.h
+++ leaderboard.h
@@ -1,7 +1,7 @@
 #ifndef LEADERBOARD_H
 #define LEADERBOARD_H
 
-#define LBOARD_DEBUG
+//#define LBOARD_DEBUG
 
 #define MAX_DISPLAY_ENTRY 20
 #define MAX_LBOARD_RESULT 10
@@ -61,9 +61,6 @@ void update_lboard( int lboard_type, CHAR_DATA *ch, int current, int increment )
 LBOARD_ENTRY *lboard_entry_new( char *name, int value );
 void do_lboard( CHAR_DATA *ch, char *argument);
 LBOARD* lboard_load_from_file( FILE *fp, int type );
-void reset_daily_lboards();
-void reset_weekly_lboards();
-void reset_monthly_lboards();
 void reset_lboard( LBOARD **board);
 void lboard_init();
 
