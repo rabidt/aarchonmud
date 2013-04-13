8fa4a9f06fbe5b62bab4ea3a898872f5095e8471
bobble <bobble@localhost>
2013-04-13 15:06:54 +1200

    allow gender-specifc races for both genders; causes gender-change while in that form

--- comm.c
+++ comm.c
@@ -2531,13 +2531,6 @@ void show_string(struct descriptor_data *d, char *input)
 }
 
 
-/* quick sex fixer */
-void fix_sex(CHAR_DATA *ch)
-{
-	if (ch->sex < 0 || ch->sex > 2)
-		ch->sex = IS_NPC(ch) ? 0 : ch->pcdata->true_sex;
-}
-
 /* non-trigger act */
 void nt_act ( const char *format, CHAR_DATA *ch, const void *arg1, const void *arg2,
 	      int type )
