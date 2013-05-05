a9d07fca91d9d46cd73e5ba78c2440325bc8cec1
Vodur <17693233+Odoth@users.noreply.github.com>
2013-05-05 13:01:51 +0400

    Testing packages.

---
+++ lua/vodur/test1.lua
@@ -0,0 +1,13 @@
+function poopo(char, mob, room)
+    say("Starting poopo")
+    if char==mob then
+      say("char==mob, silly")
+    else
+      say(ch.name .. " is NOT " .. mob.name)
+    end
+
+    say(room.name)
+end
+
+
+
