f7cb3517d5c7b4c96e2880be3ca27a0401640efd
m256ada <m256ada@aarchonmud>
2013-05-24 20:06:53 -0700

    8 + participants*2 qp bonus for warfare

--- warfare.c
+++ warfare.c
@@ -120,7 +120,8 @@ void auto_war(void)
     war.on = TRUE;
     war.started = FALSE;
     war.combatants = 0;
-    war.war_time_left = 4;
+//    war.war_time_left = 4;
+    war.war_time_left = 1;
     war.first_combatant = NULL;
     war.reward = 4;
     war.owner = 0;
@@ -232,7 +233,8 @@ void proc_startwar( CHAR_DATA *ch, char *argument, bool pay )
     war.on = TRUE;
     war.started = FALSE;
     war.combatants = 0;
-    war.war_time_left = 3;
+//    war.war_time_left = 3;
+    war.war_time_left = 1;
     war.first_combatant = NULL;
 
     if ( !pay )
@@ -513,6 +515,7 @@ void war_update( void )
 
         sprintf( buf, "The battle begins with %d combatants in the war!\n\r", war.combatants );
         warfare( buf );
+        war.total_combatants = war.combatants;
         war.started = TRUE;
         for ( d = descriptor_list; d != NULL; d = d->next )
         {
@@ -737,6 +740,7 @@ void war_remove( CHAR_DATA *ch, bool killed )
 {
     DESCRIPTOR_DATA *d;
     char buf[MSL];
+    int joinbonus;
     
     if (IS_NPC(ch) || !IS_SET( ch->act, PLR_WAR ) )
         return;
@@ -781,6 +785,23 @@ void war_remove( CHAR_DATA *ch, bool killed )
 	char_to_room( ch, get_room_index(ROOM_VNUM_TEMPLE) );
     }
     do_look( ch, "" );
+    
+    /* Small reward for participation - Astark 5-24-13 */
+
+/* Why doesn't this work?
+    joinbonus = UMAX(8 + (war.total_combatants*2), 12); */
+
+    joinbonus = 10 + war.total_combatants*2;
+
+    if (joinbonus < 20)
+        joinbonus = joinbonus;
+    else
+        joinbonus = 20;
+
+    sprintf( buf, "You are awarded %d quest points for your bravery!\n\r", joinbonus);
+    send_to_char(buf,ch);
+ 
+    ch->pcdata->questpoints += joinbonus;
 
     if (!killed)
     {
