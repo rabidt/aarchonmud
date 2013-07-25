953fdef9cabe5a0e5d3e5fa46ef03295e7ebfe7b
bobble <bobble@localhost>
2013-07-25 12:22:29 +1200

    enabled checking whether mprog is running

--- merc.h
+++ merc.h
@@ -4426,6 +4426,7 @@ bool    saves_spell args( ( int level, CHAR_DATA *victim, int dam_type ) );
 bool obj_cast_spell( int sn, int level, CHAR_DATA *ch, OBJ_DATA *obj, char *arg );
 
 /* mob_prog.c */
+bool    is_mprog_running  args( (void) );
 void    program_flow    args( ( char *text, bool is_lua, int vnum, char *source, CHAR_DATA *mob, CHAR_DATA *ch,
 				const void *arg1, sh_int arg1type,
                 const void *arg2, sh_int arg2type) );
