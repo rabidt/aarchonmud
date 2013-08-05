e08085ce56345f12a19b7484d18fafa5ab491b97
Vodur <Vodur@localhost>
2013-08-05 09:06:28 +0400

    Should be good

--- lua/leaderboard.lua
+++ lua/leaderboard.lua
@@ -1,47 +1,63 @@
 local dbg=true
-local lb_tables={
-    daily={
-        timeout=0,
-        tables={
-            mkill={name="Mob Kills", entries={} },
+
+local function new_lbtables(typ)
+    local rtn={}
+    if typ=="daily" then
+        rtn={mkill={name="Mob Kills", entries={} },
             qcomp={name="Quest Completed", entries={} },
             qpnt={name="Quest Points", entries={} },
             qfail={name="Quests Failed", entries={} },
-            level={name="Levels Gained", entries={} } 
-        } 
-    },
-    weekly={
-        timeout=0,
-        tables={
-            mkill={name="Mob Kills", entries={} },
+            level={name="Levels Gained", entries={} }}
+    elseif typ=="weekly" then
+        rtn={mkill={name="Mob Kills", entries={} },
             qcomp={name="Quest Completed", entries={} },
             qpnt={name="Quest Points", entries={} },
             qfail={name="Quests Failed", entries={} },
-            level={name="Levels Gained", entries={} } 
-        } 
-    },
-    monthly={
-        timeout=0,
-        tables={
-            mkill={name="Mob Kills", entries={} },
+            level={name="Levels Gained", entries={} }}
+    elseif typ=="monthly" then
+        rtn={mkill={name="Mob Kills", entries={} },
             qcomp={name="Quest Completed", entries={} },
             qpnt={name="Quest Points", entries={} },
             qfail={name="Quests Failed", entries={} },
-            level={name="Levels Gained", entries={} } 
-        } 
-    },
-    overall={
-        timeout=0,
-        tables={
-            mkill={name="Mob Kills", entries={} },
+            level={name="Levels Gained", entries={} }}
+    elseif typ=="overall" then
+        rtn={mkill={name="Mob Kills", entries={} },
             qcomp={name="Quest Completed", entries={} },
             bhd={name="Beheads", entries={} },
             wkill={name="War Kills", entries={} },
             expl={name="Rooms Explored", entries={} },
             qfail={name="Quests Failed", entries={} },
-            pkill={name="Player Kills", entries={} }
-        } 
+            pkill={name="Player Kills", entries={} }}
+    else
+        error("new_lbtables, invalid typ argument")
+    end
+
+    return rtn
+end
+
+local lb_tables={
+    daily={
+        timeout=0,
+        tables=new_lbtables("daily")
+    },
+    weekly={
+        timeout=0,
+        tables=new_lbtables("weekly")
+    },
+    monthly={
+        timeout=0,
+        tables=new_lbtables("monthly")
+    },
+    overall={
+        timeout=0,
+        tables=new_lbtables("overall")
     }
+}
+
+local lh_tables={
+    daily={},
+    weekly={},
+    monthly={}
 }
 
 local types=
@@ -131,6 +147,13 @@ function save_lboards()
   f:write(out)
 
   f:close()
+
+  -- Save lboard history too
+  f=io.open( "lhistory.lua", "w")
+  out,saved=serialize.save("lh_tables",lh_tables)
+  f:write(out)
+
+  f:close()
 end
 
 function load_lboards()
@@ -140,6 +163,15 @@ function load_lboards()
   end
 
   lb_tables=f()
+
+  -- Load lboard history too
+  f=loadfile("lhistory.lua")
+  if f==nil then
+    return
+  end
+
+  lh_tables=f()
+
   check_lboard_reset()
 end
     
@@ -160,11 +192,88 @@ local function print_entries(entries, ch, maxent)
   return found
 end
 
+local function print_tables(tables, ch, typefilter)
+    for ttyp,tbl in pairs(tables) do
+        if typefilter==nil or typefilter==ttyp then
+            sendtochar(ch, tbl.name .. "\r\n")
+            if not(print_entries(tbl.entries,ch, 20)) then
+              local ind=find_in_lboard(tbl.entries,ch.name)
+              if ind then
+                sendtochar(ch, string.format("{W%3d: %-25s %10d{x\r\n", ind, tbl.entries[ind].chname, tbl.entries[ind].value))
+              else
+                sendtochar(ch, "No score for " .. ch.name .. "\r\n") 
+              end
+            end
+        end
+    end
+end
+
+local function print_interval(intrvl, ch, typefilter)
+    if not(intrvl.timeout==0) then
+        sendtochar(ch, "Ending: " .. os.date("%X %x ",intrvl.timeout) .. "\r\n")
+    end
+
+    print_tables(intrvl.tables,ch,typefilter)
+end
+
+function do_lhistory( ch, argument)
+    local interval_args={ "daily", "weekly", "monthly", "overall" }
+    -- types represents type args
+    local interval
+    local typ=nil
+    local ind=nil
+    local args=string.gmatch(argument, "%S+")
+    -- Parse the arguments
+    for v in args do
+      local found
+      for _,w in pairs(interval_args) do
+        if string.find(w, v) then
+          interval=w
+          found=true
+          break
+        end
+      end
+      if not found then
+        for _,w in pairs(types) do
+          if string.find(w, v) then
+            typ=w
+            found=true
+            break
+          end
+        end
+      end
+      if not found then
+        if not(tonumber(v)==nil) then
+          ind=tonumber(v)
+          found=true
+          break
+        end
+      end 
+    end
+
+
+    -- print applicable tables
+    for k,tbl in pairs(lh_tables) do
+      if interval==nil or interval==k then
+        sendtochar(ch, k:upper() .. " LEADERBOARD HISTORY\n\r")
+        for i,v in ipairs(tbl) do
+          if ind==nil or ind==i then
+            sendtochar(ch, "Entry #" .. i .. "\n\r")
+            print_interval(v, ch, typ)
+            sendtochar(ch, "\n\r")
+          end
+        end
+      end
+    end
+
+    return 
+end
+
 function do_lboard( ch, argument)
     local interval_args={ "daily", "weekly", "monthly", "overall" }
     -- types represents type args
     local interval
-    local typ
+    local typ=nil
     local args=string.gmatch(argument, "%S+")
     -- Parse the arguments
     for v in args do
@@ -189,26 +298,9 @@ function do_lboard( ch, argument)
     -- print applicable tables
     for k,tbl in pairs(lb_tables) do
       if interval==nil or interval==k then
-        sendtochar(ch, k .. "\r\n")
-        if not(k=="overall") then
-          sendtochar(ch, os.date("%X %x ",tbl.timeout) .. "\r\n")
-        end
-
-        for l,tb in pairs(tbl.tables) do
-          if typ==nil or typ==l then
-            sendtochar(ch, tb.name .. "\r\n")
-            sendtochar(ch, string.format("Rank %-25s %10s\r\n", "Player", "Value"))
-            local found=print_entries(tb.entries, ch, 20)
-            if not found then
-              ind=find_in_lboard(tb.entries,ch.name)
-              if ind then
-                sendtochar(ch, string.format("{W%3d: %-25s %10d{x\r\n", ind, tb.entries[ind].chname, tb.entries[ind].value))
-              else
-                sendtochar(ch, "No score for " .. ch.name .. "\r\n")
-              end
-            end
-          end
-        end
+        sendtochar(ch, k:upper() .. " LEADERBOARD\n\r")
+        print_interval(tbl, ch, typ)
+        sendtochar(ch, "\n\r")
       end
     end
 
@@ -231,15 +323,35 @@ local function reset_monthly()
   lb_tables.monthly.timeout=os.time{year=now.year, month=now.month+1,day=now.day}
 end
 
+local function make_result(tbl,typ)
+    local rslt={}
+    rslt.timeout=tbl.timeout
+    rslt.tables=tbl.tables
+
+    tbl.tables=nil
+    tbl.tables=new_lbtables(typ)
+
+    local tmp=lh_tables[typ] or {}
+    table.insert(tmp, rslt)
+    lh_tables[typ]=tmp
+    
+    if #lh_tables[typ]>10 then
+        table.remove(lh_tables[typ],1)
+    end
+end
+
 function check_lboard_reset()
   for k,tbl in pairs(lb_tables) do
     if os.time()>tbl.timeout then
       if k=="overall" then
       elseif k=="daily" then
+        make_result(tbl,k)
         reset_daily()
       elseif k=="weekly" then
+        make_result(tbl,k)
         reset_weekly()
       elseif k=="monthly" then
+        make_result(tbl,k)
         reset_monthly()
       end
     end
