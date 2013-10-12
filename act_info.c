ead4c26442c70fde9e66eef10d6819c13ddfe6b3
Vodur <17693233+Odoth@users.noreply.github.com>
2013-10-12 23:54:45 +0400

    Imms can see closed doors.

--- act_info.c
+++ act_info.c
@@ -2122,13 +2122,19 @@ void do_exits( CHAR_DATA *ch, char *argument )
         if ( ( pexit = ch->in_room->exit[door] ) != NULL
             &&   pexit->u1.to_room != NULL
             &&   can_see_room(ch,pexit->u1.to_room) 
-            &&   !IS_SET(pexit->exit_info, EX_CLOSED) )
+            &&   (!IS_SET(pexit->exit_info, EX_CLOSED) || IS_IMMORTAL(ch) ) )
         {
             found = TRUE;
             if ( fAuto )
             {
-                strcat( buf, " " );
-                strcat( buf, dir_name[door] );
+                if (IS_SET(pexit->exit_info, EX_CLOSED))
+                {
+                    sprintf( buf, "%s (%s)", buf, dir_name[door] );
+                }
+                else
+                {
+                    sprintf( buf, "%s %s", buf, dir_name[door] );
+                }
             }
             else
             {
