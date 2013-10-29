67bc55662a0e08791c7231696c04f95395893942
bobble <bobble@localhost>
2013-10-30 09:24:22 +1300

    upated default groups for mage, cleric, thief; added new groups stealth, larceny, robbery

--- merc.h
+++ merc.h
@@ -266,7 +266,7 @@ bool is_questeq( OBJ_DATA *obj );
  * Adjust the pulse numbers to suit yourself.
  */
 #define MAX_SKILL         425
-#define MAX_GROUP          77 /* accurate jan 2013 */
+#define MAX_GROUP          80 /* accurate oct 2013 */
 #define MAX_IN_GROUP       15
 #define MAX_ALIAS          35
 #define MAX_CLASS          15
