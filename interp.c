6aae5b81b6b6125a9c27d354f2e99b1ac578dcc7
m256ada <m256ada@aarchonmud>
2013-10-31 15:57:22 -0700

    works*

--- interp.c
+++ interp.c
@@ -226,6 +226,7 @@ const   struct  cmd_type    cmd_table   [] =
     { "wimpy",      do_wimpy,   POS_DEAD,    0,  LOG_NORMAL, 1, FALSE, FALSE  },
     { "triggersafe", do_trigger_safe, POS_DEAD,    0,  LOG_NORMAL, 1, FALSE, FALSE  },
     { "noexp",      do_noexp,       POS_DEAD,        0,  LOG_NORMAL, 1, FALSE, FALSE },
+    { "nohelp",     do_nohelp,      POS_DEAD,        0,  LOG_NORMAL, 1, FALSE, FALSE },
     
    /*
     * Communication commands.
