c955bce575483de676fbab57e535d9da5190653c
Vodur <Vodur@localhost>
2013-11-12 01:05:30 +0300

    Started C approach but switching to lua.

--- lua_object_type.h
+++ lua_object_type.h
@@ -11,15 +11,6 @@
 typedef void * CHECK_FUN ( struct obj_type *self, lua_State *LS, int index );
 typedef int PROP_FUNC( lua_State *LS, void *gobj );
 
-typedef struct luahelp_topic
-{
-    char *key;
-    char *summary;
-    char *arguments;
-    char *syntax;
-    char *notes;
-} LUAHELP_TOPIC;
-
 typedef struct prop_type
 {
     char *field;
