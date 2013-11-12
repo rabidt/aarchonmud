18c5d8858960998b8e838b2e7c393ea6a6c566c9
Vodur <17693233+Odoth@users.noreply.github.com>
2013-11-12 06:16:26 +0300

    Just gonna do the help in obj_type file.

--- lua_object_type.h
+++ lua_object_type.h
@@ -1,23 +1,8 @@
 #ifndef LUA_OBJECT_TYPE_H
 #define LUA_OBJECT_TYPE_H
 
-#define NO_OFF -1
-
-#define PTYPE_NONE 0
-#define PTYPE_INT 1
-#define PTYPE_STR 2
-#define PTYPE_FUN 3
-
 typedef void * CHECK_FUN ( struct obj_type *self, lua_State *LS, int index );
-typedef int PROP_FUNC( lua_State *LS, void *gobj );
-
-typedef struct prop_type
-{
-    char *field;
-    PROP_FUNC *func;
-    int security;
-    LUAHELP_TOPIC *help;
-} LUA_PROP_TYPE;
+typedef struct prop_type LUA_PROP_TYPE;
 
 /* base functionality for lua object types */
 typedef struct obj_type
