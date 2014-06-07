75124215e5774a66a9cd4f0ccf8c572a443d19a8
m256ada <m256ada@aarchonmud>
2014-06-07 09:03:04 -0700

    disarmed mobs will re-equip weapons. See update.c

--- update.c
+++ update.c
@@ -948,19 +948,20 @@ void mobile_update( void )
            that a few rare mobs are holding weapons that they aren't meant to
            equip, but we'll cross that bridge if and when we come to it 
            - Astark 1-7-13 
+           - 6-7-14, enabling this code finally - Astark */
 
            if ( ch->position == POS_FIGHTING && !number_bits(2))
            {
-           for ( obj = ch->carrying; obj != NULL; obj = obj_next )
-           {
-           if (obj->item_type == ITEM_WEAPON)
-           {
-           do_wear(ch,obj->name);
-           break;
+               for ( obj = ch->carrying; obj != NULL; obj = obj_next )
+               {
+                   if (obj->item_type == ITEM_WEAPON)
+                   {
+                       do_wear(ch,obj->name);
+                       break;
+                   }
+               }
            }
-           }
-           }
-         */
+
 
         /* That's all for sleeping / busy monster, and empty zones */
         if ( ch->position != POS_STANDING )
