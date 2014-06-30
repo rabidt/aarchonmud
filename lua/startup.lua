77e3ec259fc27f0bb7e2d3b8fae6d6c138cb919f
Vodur <Vodur@localhost>
2014-06-30 00:15:47 -0700

    Backed out changeset: b9d24475c3c0

--- lua/startup.lua
+++ lua/startup.lua
@@ -10,9 +10,6 @@ envtbl={} -- game object script environments
 interptbl={} -- key is game object pointer, table of desc=desc pointer, name=char name
 delaytbl={} -- used on the C side mostly
 
-cleanup={}
-validuds={}
-
 function UdCnt()
     local reg=debug.getregistry()
 
