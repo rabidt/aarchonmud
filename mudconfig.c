a71002ce1ce9cc8df424895230a36b173f824296
bobble <bobble@localhost>
2014-07-25 11:05:26 +1200

    added gold multiplier to mudconfig

--- mudconfig.c
+++ mudconfig.c
@@ -13,6 +13,11 @@ bool cfg_enable_qp_mult=FALSE;
 float cfg_qp_mult;
 const float cfg_qp_mult_default=1;
 
+bool cfg_show_gold_mult=FALSE;
+bool cfg_enable_gold_mult=FALSE;
+float cfg_gold_mult;
+const float cfg_gold_mult_default=1;
+
 char *cfg_word_of_day;
 const char *cfg_word_of_day_default="bananahammock";
 
@@ -24,6 +29,9 @@ CFG_DATA_ENTRY mudconfig_table[] =
     { "show_qp_mult",       CFG_BOOL,   &cfg_show_qp_mult,      NULL },
     { "enable_qp_mult",     CFG_BOOL,   &cfg_enable_qp_mult,    NULL },
     { "qp_mult",            CFG_FLOAT,  &cfg_qp_mult,           &cfg_qp_mult_default },
+    { "show_gold_mult",     CFG_BOOL,   &cfg_show_gold_mult,    NULL },
+    { "enable_gold_mult",   CFG_BOOL,   &cfg_enable_gold_mult,  NULL },
+    { "gold_mult",          CFG_FLOAT,  &cfg_gold_mult,         &cfg_gold_mult_default },
     { "word_of_day",        CFG_STRING, &cfg_word_of_day,       &cfg_word_of_day_default},
     { NULL, NULL, NULL, NULL }
 };
