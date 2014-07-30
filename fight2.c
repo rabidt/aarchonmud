3d55a55100a4461868baec1983dca4d2f5383b8d
Henning Koehler <henning.koehler.nz@gmail.com>
2014-07-30 19:42:15 +1200

    typo fix: gelly -> jelly

--- fight2.c
+++ fight2.c
@@ -4450,8 +4450,8 @@ void do_mindflay( CHAR_DATA *ch, char *argument )
 
   if ( number_bits(2) == 0 )
   {
-    act( "Your mind turns into gelly.", ch, NULL, victim, TO_VICT);
-    act( "$N's mind turns into gelly.", ch, NULL, victim, TO_CHAR);
+    act( "Your mind turns into jelly.", ch, NULL, victim, TO_VICT);
+    act( "$N's mind turns into jelly.", ch, NULL, victim, TO_CHAR);
     affect_join(victim,&af);
     if ( confuse )
     {
