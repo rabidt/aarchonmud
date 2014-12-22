c657fb57d395e0894b90736584aeff337e7e500e
bobble <bobble@localhost>
2014-12-23 08:38:54 +1300

    added mp_greet_trigger check on mob transfer as well

--- act_move.c
+++ act_move.c
@@ -444,13 +444,11 @@ int move_char( CHAR_DATA *ch, int door, bool follow )
        ap_renter_trigger( ch );
        rp_enter_trigger( ch );
        op_greet_trigger( ch );
+       mp_greet_trigger( ch );
    }
 
    if ( IS_NPC( ch ) && HAS_TRIGGER( ch, TRIG_ENTRY ) )
        mp_percent_trigger( ch, NULL, NULL, 0, NULL, 0, TRIG_ENTRY );
-   if ( !IS_NPC( ch ) )
-       mp_greet_trigger( ch );
-
 
    /* mprog might have moved the char */
    if ( ch->in_room != to_room )
