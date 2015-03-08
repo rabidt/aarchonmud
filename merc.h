97af1f7469d1737690e68f65bb89d272b37b95de
Henning Koehler <henning.koehler.nz@gmail.com>
2015-03-09 09:41:10 +1300

    practiced field exp won't be split into real and field exp again

--- merc.h
+++ merc.h
@@ -5144,6 +5144,7 @@ void clear_area_quests( CHAR_DATA *ch, AREA_DATA *area );
 void    war_update      args( ( void ) ); 
 void    advance_level   args( ( CHAR_DATA *ch, bool hide ) );
 void    gain_exp    args( ( CHAR_DATA *ch, int gain ) );
+void    update_pc_level( CHAR_DATA *ch );
 bool    starvation_immune( CHAR_DATA *ch );
 void    gain_condition  args( ( CHAR_DATA *ch, int iCond, int value ) );
 void    update_handler  args( ( void ) );
