dd9bbf0f34d1983633f905a30ec7181fbbac52b8
Henning Koehler <henning.koehler.nz@gmail.com>
2015-04-09 12:31:39 +1200

    added exploit weakness skill and slayer subclass

--- db.c
+++ db.c
@@ -376,7 +376,7 @@ sh_int  gsn_bullet_rain;
 sh_int  gsn_precise_shot;
 sh_int  gsn_holy_avenger;
 sh_int  gsn_divine_retribution;
-
+sh_int  gsn_exploit_weakness;
 
 sh_int  gsn_laughing_fit;
 sh_int  gsn_deaths_door;
