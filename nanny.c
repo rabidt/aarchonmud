11e1c185f80b6df113b70c1837e625a6056d04dc
bobble <bobble@localhost>
2015-09-19 15:43:49 +1200

    new characters start with wimpy and calm at 20

--- nanny.c
+++ nanny.c
@@ -1956,7 +1956,9 @@ void enter_game ( DESCRIPTOR_DATA *d )
 	    SET_BIT(ch->act, PLR_AUTOLOOT);
 	    SET_BIT(ch->act, PLR_AUTOGOLD);
 	    SET_BIT(ch->act, PLR_AUTOSPLIT);
-            SET_BIT(ch->act, PLR_AUTOASSIST);
+        SET_BIT(ch->act, PLR_AUTOASSIST);
+        ch->wimpy = 20;
+        ch->calm = 20;
 
 	    sprintf( buf, "the %s",
 		     title_table [ch->class] [(ch->level+4-(ch->level+4)%5)/5]);
