a9cd04b964c6ad63ced9198feff2398a592dc06e
bobble <bobble@localhost>
2015-10-19 14:35:51 +1300

    adjusted permcast cost for stone skin (special duration)

--- merc.h
+++ merc.h
@@ -3353,6 +3353,7 @@ extern sh_int  gsn_poison;
 extern sh_int  gsn_sleep;
 extern sh_int  gsn_fly;
 extern sh_int  gsn_sanctuary;
+extern sh_int  gsn_stone_skin;
 extern sh_int  gsn_necrosis;
 extern sh_int  gsn_ritual;
 extern sh_int  gsn_word_of_recall;
