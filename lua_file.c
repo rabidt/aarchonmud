4bc01ffcf7da194acdab7734cf4d47540f4cc58b
Vodur <17693233+Odoth@users.noreply.github.com>
2015-11-01 22:22:16 +0300

    POC with mobbles.

--- lua_file.c
+++ lua_file.c
@@ -7,21 +7,23 @@
 #include "lua_main.h"
 #include "lua_file.h"
 
+/* just some laziness here */
+#define LS g_mud_LS
 
 /* LStbl section */
-void LStbl_create( lua_State *LS, LStbl *tbl )
+void LStbl_create( LStbl *tbl )
 {
     lua_newtable( LS );
     tbl->ref=lua_gettop( LS );
 }
 
-void LStbl_release( lua_State *LS, LStbl *tbl )
+void LStbl_release( LStbl *tbl )
 {
     lua_remove( LS, tbl->ref);
     tbl->ref = LUA_NOREF;
 } 
 
-void LStbl_save( lua_State *LS, LStbl *tbl, const char *filename )
+void LStbl_save( LStbl *tbl, const char *filename )
 {
     lua_getglobal( LS, "LSAVE_table" );
     lua_pushvalue( LS, tbl->ref );
@@ -29,7 +31,7 @@ void LStbl_save( lua_State *LS, LStbl *tbl, const char *filename )
     lua_call( LS, 2, 0 );
 }
 
-void LStbl_kv_str( lua_State *LS, LStbl *tbl, const char *key, const char *val)
+void LStbl_kv_str( LStbl *tbl, const char *key, const char *val)
 {
     lua_newtable( LS );
     lua_pushstring( LS, key );
@@ -39,7 +41,7 @@ void LStbl_kv_str( lua_State *LS, LStbl *tbl, const char *key, const char *val)
     lua_rawseti( LS, tbl->ref, lua_objlen( LS, tbl->ref) + 1 );
 }
 
-void LStbl_kv_bool( lua_State *LS, LStbl *tbl, const char *key, const bool val)
+void LStbl_kv_bool( LStbl *tbl, const char *key, const bool val)
 {
     lua_newtable( LS );
     lua_pushstring( LS, key );
@@ -49,7 +51,7 @@ void LStbl_kv_bool( lua_State *LS, LStbl *tbl, const char *key, const bool val)
     lua_rawseti( LS, tbl->ref, lua_objlen( LS, tbl->ref) + 1 );
 }
 
-void LStbl_kv_int( lua_State *LS, LStbl *tbl, const char *key, const int val)
+void LStbl_kv_int( LStbl *tbl, const char *key, const int val)
 {
     lua_newtable( LS );
     lua_pushstring( LS, key );
@@ -59,7 +61,7 @@ void LStbl_kv_int( lua_State *LS, LStbl *tbl, const char *key, const int val)
     lua_rawseti( LS, tbl->ref, lua_objlen( LS, tbl->ref) + 1 );
 }
 
-void LStbl_kv_arr( lua_State *LS, LStbl *tbl, const char *key, LSarr *arr)
+void LStbl_kv_arr( LStbl *tbl, const char *key, LSarr *arr)
 {
     lua_newtable( LS );
     lua_pushstring( LS, key );
@@ -68,22 +70,48 @@ void LStbl_kv_arr( lua_State *LS, LStbl *tbl, const char *key, LSarr *arr)
     lua_setfield( LS, -2, "LVAL" );
     lua_rawseti( LS, tbl->ref, lua_objlen( LS, tbl->ref) + 1 );
 }
+
+void LStbl_kv_tbl( LStbl *tbl, const char *key, LStbl *subtbl )
+{
+    lua_newtable( LS );
+    lua_pushstring( LS, key );
+    lua_setfield( LS, -2, "LKEY" );
+    lua_pushvalue( LS, subtbl->ref );
+    lua_rawseti( LS, tbl->ref, lua_objlen( LS, tbl->ref) + 1 );
+}
+
+void LStbl_kv_flags( LStbl *tbl, const char *key, const struct flag_type *flag_table, const tflag f)
+{
+    LSarr flags;
+    LSarr_create( &flags );
+    LStbl_kv_arr( tbl, key, &flags );
+
+    int i=0;
+    for (i=0; flag_table[i].name; i++)
+    {
+        if (IS_SET(f, flag_table[i].bit))
+        {
+            LSarr_add_str( &flags, flag_table[i].name);
+        }
+    }
+    LSarr_release( &flags);
+}
 /* end LStbl section */
 
 /* LSarr section */
-void LSarr_create( lua_State *LS, LSarr *arr )
+void LSarr_create( LSarr *arr )
 {
     lua_newtable( LS );
     arr->ref=lua_gettop( LS );
 }
 
-void LSarr_release( lua_State *LS, LSarr *arr )
+void LSarr_release( LSarr *arr )
 {
     lua_remove( LS, arr->ref );
     arr->ref=LUA_NOREF;
 }
 
-void LSarr_add_str( lua_State *LS, LSarr *arr, const char *val )
+void LSarr_add_str( LSarr *arr, const char *val )
 {
     lua_newtable( LS );
     lua_pushstring( LS, val );
@@ -91,7 +119,7 @@ void LSarr_add_str( lua_State *LS, LSarr *arr, const char *val )
     lua_rawseti( LS, arr->ref, lua_objlen( LS, arr->ref) + 1 );
 }
 
-void LSarr_add_int( lua_State *LS, LSarr *arr, const int val )
+void LSarr_add_int( LSarr *arr, const int val )
 {
     lua_newtable( LS );
     lua_pushinteger( LS, val );
@@ -99,7 +127,7 @@ void LSarr_add_int( lua_State *LS, LSarr *arr, const int val )
     lua_rawseti( LS, arr->ref, lua_objlen( LS, arr->ref) + 1 );
 }
 
-void LSarr_add_tbl( lua_State *LS, LSarr *arr, LStbl *tbl )
+void LSarr_add_tbl( LSarr *arr, LStbl *tbl )
 {
     lua_newtable( LS );
     lua_pushvalue( LS, tbl->ref );
@@ -110,7 +138,7 @@ void LSarr_add_tbl( lua_State *LS, LSarr *arr, LStbl *tbl )
 /* end LSarr section */
 
 /* LLtbl section */
-void LLtbl_load( lua_State *LS, LLtbl *tbl, const char *filename )
+void LLtbl_load( LLtbl *tbl, const char *filename )
 {
     lua_getglobal( LS, "LLOAD_table" );
     lua_pushstring( LS, filename );
@@ -118,13 +146,13 @@ void LLtbl_load( lua_State *LS, LLtbl *tbl, const char *filename )
     tbl->ref = lua_gettop(LS);
 }
 
-void LLtbl_release( lua_State *LS, LLtbl *tbl )
+void LLtbl_release( LLtbl *tbl )
 {
     lua_remove( LS, tbl->ref );
     tbl->ref=LUA_NOREF;
 }
 
-int LLtbl_get_kv_int( lua_State *LS, LLtbl *tbl, const char *key )
+int LLtbl_get_kv_int( LLtbl *tbl, const char *key )
 {
     int rtn;
     lua_getfield( LS, tbl->ref, key );
@@ -133,7 +161,7 @@ int LLtbl_get_kv_int( lua_State *LS, LLtbl *tbl, const char *key )
     return rtn;  
 }
 
-const char *LLtbl_get_kv_str( lua_State *LS, LLtbl *tbl, const char *key) 
+const char *LLtbl_get_kv_str( LLtbl *tbl, const char *key) 
 {
     const char *rtn;
     lua_getfield( LS, tbl->ref, key );
@@ -142,14 +170,14 @@ const char *LLtbl_get_kv_str( lua_State *LS, LLtbl *tbl, const char *key)
     return rtn;    
 }
 
-void LLtbl_get_kv_tbl( lua_State *LS, LLtbl *tbl, const char *key, LLtbl *subtbl)
+void LLtbl_get_kv_tbl( LLtbl *tbl, const char *key, LLtbl *subtbl)
 {
     lua_getfield( LS, tbl->ref, key );
     luaL_checktype( LS, -1, LUA_TTABLE );
     subtbl->ref=lua_gettop( LS );
 }
 
-const char *LLtbl_get_iv_str( lua_State *LS, LLtbl *tbl, int index)
+const char *LLtbl_get_iv_str( LLtbl *tbl, int index)
 {
     const char *rtn;
     lua_rawgeti( LS, tbl->ref, index );
@@ -158,7 +186,7 @@ const char *LLtbl_get_iv_str( lua_State *LS, LLtbl *tbl, int index)
     return rtn;
 }
 
-int LLtbl_get_iv_int( lua_State *LS, LLtbl *tbl, int index)
+int LLtbl_get_iv_int( LLtbl *tbl, int index)
 {
     int rtn;
     lua_rawgeti( LS, tbl->ref, index);
@@ -167,14 +195,14 @@ int LLtbl_get_iv_int( lua_State *LS, LLtbl *tbl, int index)
     return rtn;
 }
 
-void LLtbl_get_iv_tbl( lua_State *LS, LLtbl *tbl, int index, LLtbl *subtbl)
+void LLtbl_get_iv_tbl( LLtbl *tbl, int index, LLtbl *subtbl)
 {
     lua_rawgeti( LS, tbl->ref, index);
     luaL_checktype( LS, -1, LUA_TTABLE );
     subtbl->ref=lua_gettop( LS );
 }
     
-bool LLtbl_i_exists( lua_State *LS, LLtbl *tbl, int index )
+bool LLtbl_i_exists( LLtbl *tbl, int index )
 {
     bool rtn;
     lua_rawgeti( LS, tbl->ref, index );
