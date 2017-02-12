892654d1df206e26b890b96fb8407af1b3bcd931
Henning Koehler <henning.koehler.nz@gmail.com>
2017-02-12 17:06:07 +1300

    fixed floating disc target and wear-off message

--- const.c
+++ const.c
@@ -3711,9 +3711,9 @@ struct  skill_type
     {   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3 }, 1, 1,
     { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 },
     STAT_NONE, STAT_NONE, STAT_NONE,
-    spell_floating_disc,    TAR_IGNORE_OBJ,     POS_STANDING,
+    spell_floating_disc, TAR_CHAR_SELF, POS_STANDING,
     &gsn_floating_disc, 40, 0, 24, DUR_EXTREME,
-    "",         "!Floating disc!",  ""
+    "", "The disc floating next to you disappears.", ""
     },
 
     {
