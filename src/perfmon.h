828606d915c391135bba2f09414dca6e0aab6972
Vodur <Vodur@localhost>
2017-03-19 20:42:52 -0400

    Add support to measure execution times.

--- src/perfmon.h
+++ src/perfmon.h
@@ -1,3 +1,6 @@
+struct PERF_track;
+
+
 void PERF_log_pulse(double val);
 const char *PERF_repr();
 
@@ -9,6 +12,18 @@ typedef struct
     int count;
     double *vals;
 } PERF_data;
+
+struct PERF_meas_s;
+void PERF_meas_reset();
+void PERF_meas_start(struct PERF_meas_s **tr, const char *tag);
+void PERF_meas_end(struct PERF_meas_s **tr);
+const char *PERF_meas_repr();
+
+#define PERF_MEASURE(name, section) \
+struct PERF_meas_s *_ms_ ## name;\
+PERF_meas_start(& _ms_ ## name, #name);\
+section \
+PERF_meas_end(& _ms_ ## name);
 
 
 #ifdef UNITTEST
