5d6ef15dfd9a33f8f90cd63d849ac224b85aad9d
Vodur <Vodur@localhost>
2019-05-10 23:37:42 -0700

    Move dxport_handler into main repo.

---
+++ tools/dxport_handler/src/config.py
@@ -0,0 +1,7 @@
+import os
+
+
+DB_PATH = os.environ.get("DX_DB_PATH")
+PLAYER_DIR = os.environ.get("DX_PLAYER_PATH")
+SOCK_PATH = os.environ.get("DX_SOCK_PATH")
+LOG_LEVEL = os.environ.get("DX_LOG_LEVEL")
