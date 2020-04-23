652331fd50df6b91a9e2ec5f916f86abaf309fa3
Vodur <clayton.richey@gmail.com>
2020-04-23 11:19:13 -0700

    Add tag reset for storage boxes.

--- data/user/bastion/box.lua
+++ data/user/bastion/box.lua
@@ -11,9 +11,45 @@ Syntax:
 
     tag desc                 - List all box numbers and their currend descriptions.
     tag desc <#> <desc>      - Set description for a box.
+
+    tag reset <#>            - Reset keywords and desc for a box.
 ]])
 end
 
+local function box_reset(room, ch, args)
+    if #args < 1 then
+        show_tag_usage(ch)
+        return
+    end
+
+    local box_num = tonumber(args[1])
+    if nil == box_num then
+        show_tag_usage(ch)
+        return
+    end
+
+    local i = 1
+    local box = nil
+    for _,v in ipairs(room.contents) do
+        if v.vnum == BOX_VNUM then
+            if i == box_num then
+                box = v
+                break
+            else
+                i = i+1
+            end
+        end
+    end
+
+    if nil == box then
+        sendtochar(ch, "There is no box #"..box_num.."\n\r")
+        return
+    end
+
+    box.name = box.proto.name
+    box.description = box.proto.description
+    sendtochar(ch, "Box #"..box_num.." keywords and desc reset.\n\r")
+end
 
 local function box_base(propname, room, ch, args)
     if #args < 1 then
@@ -42,7 +78,7 @@ local function box_base(propname, room, ch, args)
 
     local i = 1
     local box = nil
-    for _,v in pairs(room.contents) do
+    for _,v in ipairs(room.contents) do
         if v.vnum == BOX_VNUM then
             if i == box_num then
                 box = v
@@ -72,6 +108,10 @@ function box_room_trigger(room, ch, text)
     elseif args[1] == "desc" then
         table.remove(args, 1)
         box_base("description", room, ch, args)
+        return
+    elseif args[1] == "reset" then
+        table.remove(args, 1)
+        box_reset(room, ch, args)
         return
     else
         show_tag_usage(ch)
